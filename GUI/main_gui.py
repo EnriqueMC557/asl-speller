5765ebba2b674f10fa824aa90d5c8be1ff177d98
Enrique Mena-Camilo <kikemeca05@gmail.com>
2022-12-08 10:44:52 -0600

    Refactor

--- GUI/main_gui.py
+++ GUI/main_gui.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Dec  6 17:12:55 2022
 
-@author: sleyv
+@author: sleyv, emc
 """
 
 import sys
@@ -10,60 +10,67 @@ sys.path.append('../yolov5')
 
 import cv2
 import torch
+
 from statistics import mode
-from asl_speller import Ui_ASLSPELLER
-from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget
 
+from asl_speller import Ui_ASLSPELLER
 from models.common import DetectMultiBackend
+from utils.plots import Annotator, colors
 from utils.dataloaders import LoadStreams
 from utils.torch_utils import select_device
 from utils.general import check_img_size, Profile, non_max_suppression, check_imshow, scale_boxes
-from utils.plots import Annotator, colors
 
 
 weights = '../yolov5/runs/train/exp/weights/best.pt'
 
 
-class Asl_gui(QWidget):
+class AslGui(QWidget):
     def __init__(self, parent=None):
         """Inicializador de GUI."""
         QWidget.__init__(self, parent)
         self.ui = Ui_ASLSPELLER()
         self.ui.setupUi(self)
         self.timer_result = QTimer()
-        self.results = []
-        self.space = '  '
+        self.detected_classes = []
+        self.space = '-'
         self.palabra = ''
         self.dataset = None
         self.bs = None
         self.view_img = None
         self.vid_stride = 1
-        self.device = select_device('')
+
+        self.device = select_device('cpu')
         self.model = DetectMultiBackend(weights, device=self.device, dnn=False, data='data/coco128.yaml', fp16=False)
         self.stride, self.names, self.pt = self.model.stride, self.model.names, self.model.pt
-        self.imgsz = check_img_size((640, 640), s=self.stride)  # check image size
+        self.imgsz = check_img_size((640, 640), s=self.stride)
 
-        self.ui.startbutton.clicked.connect(self.iniciar)
-        self.ui.finishbutton.clicked.connect(self.finish)
-        self.ui.finishbutton.setDisabled(True)
-        self.timer_result.timeout.connect(self.updateTextResult)
+        self.ui.start_button.clicked.connect(self.start)
+        self.ui.finish_button.clicked.connect(self.finish)
+        self.ui.finish_button.setDisabled(True)
+        self.timer_result.timeout.connect(self.update_text_result)
 
-    def updateTextResult(self):
-        if len(self.results) > 0:
-            mode_class = mode(self.results)
-            letra = self.space if str(self.names[mode_class]) == 'nothing' else str(self.names[mode_class])
-            self.palabra += letra
-            self.ui.text_result.setText(self.palabra)
-            self.results = []
+    def update_text_result(self):
+        if len(self.detected_classes) == 0:
+            # Adds 'nothing' class if not detections
+            self.detected_classes.append(26)
 
-    def iniciar(self):
-        self.timer_result.start(2000)
+        mode_class = mode(self.detected_classes)
+        self.detected_classes = []
+        letra = self.space if str(self.names[mode_class]) == 'nothing' else str(self.names[mode_class])
+        if self.palabra and letra == self.space and self.palabra[-1] == self.space:
+            return
+        self.palabra += letra
+        self.ui.text_result.setText(self.palabra)
+
+    def start(self):
+        self.timer_result.start(3000)
         self.ui.text_result.setText('')
 
-        self.ui.startbutton.setDisabled(True)
-        self.ui.finishbutton.setDisabled(False)
+        self.ui.start_button.setDisabled(True)
+        self.ui.finish_button.setDisabled(False)
 
         # SetUp inference
         self.view_img = check_imshow(warn=True)
@@ -71,7 +78,7 @@ class Asl_gui(QWidget):
         self.bs = len(self.dataset)
 
         self.model.warmup(imgsz=(1 if self.pt or self.model.triton else self.bs, 3, *self.imgsz))  # warmup
-        seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+        dt = (Profile(), Profile(), Profile())
 
         self.results = []
         try:
@@ -89,22 +96,20 @@ class Asl_gui(QWidget):
 
                 # NMS
                 with dt[2]:
-                    pred = non_max_suppression(pred, 0.5, 0.45, None, False, max_det=1000)
+                    pred = non_max_suppression(pred, conf_thres=0.45, iou_thres=0.30, max_det=100)
 
                 # Process predictions
                 for i, det in enumerate(pred):  # per image
-                    seen += 1
-                    p, im0, frame = path[i], im0s[i].copy(), self.dataset.count
+                    im0, frame = im0s[i].copy(), self.dataset.count
                     annotator = Annotator(im0, line_width=3, example=str(self.names))
 
                     if len(det):
                         # Rescale boxes from img_size to im0 size
                         det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
 
-                        # Print results
-                        for c in det[:, 5].unique():
-                            detected_class = int(c)
-                            self.results.append(detected_class)
+                        # Save results
+                        for detected_class in det[:, 5].unique():
+                            self.detected_classes.append(int(detected_class))
 
                         # Write results
                         for *xyxy, conf, cls in reversed(det):
@@ -115,12 +120,12 @@ class Asl_gui(QWidget):
 
                     # Stream results
                     im0 = annotator.result()
-                    rgbImage = cv2.cvtColor(im0, cv2.COLOR_BGR2RGB)
-                    h, w, ch = rgbImage.shape
-                    bytesPerLine = ch * w
-                    convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-                    p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-                    self.ui.camera_label.setPixmap(QPixmap.fromImage(p))
+                    rgb_image = cv2.cvtColor(im0, cv2.COLOR_BGR2RGB)
+                    h, w, ch = rgb_image.shape
+                    bytes_per_line = ch * w
+                    convert_to_qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+                    pixmap = convert_to_qt_format.scaled(640, 480, Qt.KeepAspectRatio)
+                    self.ui.camera_label.setPixmap(QPixmap.fromImage(pixmap))
         except AttributeError:
             # Esto está bien, ocurre cuando se cierra la cámara
             pass
@@ -128,16 +133,16 @@ class Asl_gui(QWidget):
     def finish(self):
         self.dataset.cap.release()
         self.dataset = None
-        self.ui.camera_label.setPixmap(QPixmap("webcam.jpg"))
+        self.ui.camera_label.setPixmap(QPixmap('webcam.jpg'))
         self.timer_result.stop()
         self.palabra = ''
 
-        self.ui.startbutton.setDisabled(False)
-        self.ui.finishbutton.setDisabled(True)
+        self.ui.start_button.setDisabled(False)
+        self.ui.finish_button.setDisabled(True)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    gui = Asl_gui()
+    gui = AslGui()
     gui.show()
     sys.exit(app.exec_())
--- train_custom_yolo.py
+++ train_custom_yolo.py
@@ -4,7 +4,7 @@ import os
 if __name__ == '__main__':
     IMAGE_SIZE_PIXELS = 512
     BATCH_SIZE = 16
-    EPOCHS = 2
+    EPOCHS = 150
     DATASET_PATH = 'asl-alphabet-2'
     print('WARNING: Verify that test/train/val paths in data.yaml are in the form ../../YOUR_DATASET/test/images')
     os.system(
